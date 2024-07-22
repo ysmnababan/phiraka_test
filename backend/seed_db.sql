4316888fd804ac6351a61538ceda99d2c0cb944e
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-07-22 11:46:05 +0700

    feat: ddl and dml for table 'users'

---
+++ backend/seed_db.sql
@@ -0,0 +1,17 @@
+-- CREATE NEW TABLE FOR USER
+CREATE TABLE IF NOT EXISTS users(
+	UserID INT AUTO_INCREMENT PRIMARY KEY, 
+	Username VARCHAR(128) UNIQUE NOT NULL,
+	Password VARCHAR(54) NOT NULL,
+	CreateTime DATETIME NOT NULL
+);
+
+-- SET TIME FOR GMT+07
+SET time_zone = '+07:00';
+
+-- SEED DATA FOR USER TABLE
+INSERT INTO users (Username, Password, CreateTime) VALUES 
+('john_doe', 'password123', NOW()),
+('jane_smith', 'password456', NOW()),
+('alice_jones', 'password789', NOW());
+
